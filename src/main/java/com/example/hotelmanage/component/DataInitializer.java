7c00d305f5da8d7f43b5b60856883b3c5701af29
zxc777zxc <230811@astanait.edu.kz>
2024-02-29 01:44:46 +0600

    )

--- src/main/java/com/example/hotelmanage/component/DataInitializer.java
+++ src/main/java/com/example/hotelmanage/component/DataInitializer.java
@@ -15,7 +15,7 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Создание и сохранение сущностей Client
-        clientRepository.save(new Client("John Doe", 1));
-        clientRepository.save(new Client("Jane Doe", 2));
+//        clientRepository.save(new Client("John Doe", 1));
+//        clientRepository.save(new Client("Jane Doe", 2));
     }
 }
