807ffd1edb41609163d8fe623f549b051c38c5c1
zxc777zxc <153791686+zxc777zxc@users.noreply.github.com>
2024-02-29 01:38:26 +0600

    Add files via upload

--- src/main/java/com/example/hotelmanage/represetory/ClientRepository.java
+++ src/main/java/com/example/hotelmanage/represetory/ClientRepository.java
@@ -1,7 +1,14 @@
 package com.example.hotelmanage.represetory;
 
-import com.example.hotelmanage.model.Client;
 
-public interface ClientRepository {
+import org.springframework.data.repository.CrudRepository;
+import com.example.hotelmanage.model.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ClientRepository extends CrudRepository<Client,Long>{
     void addClient(Client client);
+
+//    Object save(Client johnDoe);
 }
