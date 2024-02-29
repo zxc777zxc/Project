fafb875aa1c2cad1eceb2a23fb67f898c1fe3e87
Ulan Bakytbekov <153665350+default175@users.noreply.github.com>
2024-02-29 09:09:16 +0600

    Delete src/main/java/com/example/hotelmanage/servise directory

--- src/main/java/com/example/hotelmanage/servise/ClientService.java
+++
@@ -1,5 +0,0 @@
-package com.example.hotelmanage.servise;
-import com.example.hotelmanage.model.Client;
-public interface ClientService {
-    void addClient(Client client);
-}
--- src/main/java/com/example/hotelmanage/servise/StaffService.java
+++
@@ -1,6 +0,0 @@
-package com.example.hotelmanage.servise;
-
-import com.example.hotelmanage.model.Staff;
-public interface StaffService {
-    void addStaff(Staff staff);
-}
